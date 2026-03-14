a17270074dc37713357eb3b80987d00b16e4ec58
ansabazys <ansabazys@gmail.com>
2026-03-14 12:13:26 +0530

    feat: add global error handler

--- apps/ingestion-service/src/server.ts
+++ apps/ingestion-service/src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { env } from "@repo/config";
 import PinoHttp from "pino-http";
 import { logger } from "./utils/logger";
@@ -8,10 +8,18 @@ const app = express();
 app.use(PinoHttp({ logger }));
 app.use(express.json());
 
-app.get("/health", (req, res) => {
-  res.json({ status: "ok" });
+app.get("/health", (req: Request, res: Response) => {
+  res.json({ status: "ok", service: "ingestion-service" });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err);
+
+  res.status(500).json({
+    error: "Internal Server Error",
+  });
 });
 
 app.listen(env.PORT, () => {
-  console.log(`Ingestion running on port ${env.PORT}`);
+  logger.info(`Ingestion running on port ${env.PORT}`);
 });
