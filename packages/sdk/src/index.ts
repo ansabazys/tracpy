cc1d3d4ff22c9175b0ef7a3d9a9f2d0cdb050730
ansabazys <ansabazys@gmail.com>
2026-03-15 15:32:18 +0530

    feat(sdk): initalize sdk

---
+++ packages/sdk/src/index.ts
@@ -0,0 +1,1 @@
+export { Tracpy } from "./tracpy";
---
+++ packages/sdk/src/types.ts
@@ -0,0 +1,7 @@
+export interface TrackEventPayload {
+  siteId: string
+  event: string
+  url: string
+  timestamp: number
+  data?: Record<string, unknown>
+}
